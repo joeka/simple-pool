977e098924da90001dc9d6030fd96d42715f2d15
DrHaid <40427630+DrHaid@users.noreply.github.com>
2025-02-18 22:38:56 +0100

    change shooting behavior

--- src/components/shooting.ts
+++ src/components/shooting.ts
@@ -6,26 +6,27 @@ export class ShootingComponent extends Component {
   declare owner: Ball;
 
   private cue: Cue;
-  private isDragged: boolean;
-  private dragStart: Vector;
-  private dragEnd: Vector;
+  private isCharging: boolean;
+  private cueDirection: Vector;
+  private clickPosition: Vector;
+  private chargingStrength: number;
   private ballRadius: number = Ball.radius;
 
   constructor() {
     super();
     this.cue = new Cue();
-    this.dragStart = Vector.Zero;
-    this.dragEnd = Vector.Zero;
-    this.isDragged = false;
+    this.isCharging = false;
+    this.cueDirection = Vector.Zero;
+    this.clickPosition = Vector.Zero;
+    this.chargingStrength = 0;
   }
 
   onAdd(owner: Ball): void {
     const engine = owner.scene?.engine;
     if (!engine) return;
 
-    engine.input.pointers.primary.on("down", this.onDragStart);
+    engine.input.pointers.primary.on("down", this.onPointerDown);
     engine.input.pointers.primary.on("move", this.onPointerMove);
-    engine.input.pointers.primary.on("up", this.onDragEnd);
     engine.currentScene.add(this.cue);
   }
 
@@ -33,43 +34,56 @@ export class ShootingComponent extends Component {
     const engine = this.owner.scene?.engine;
     if (!engine) return;
 
-    engine.input.pointers.primary.off("down", this.onDragStart);
+    engine.input.pointers.primary.off("down", this.onPointerDown);
     engine.input.pointers.primary.off("move", this.onPointerMove);
-    engine.input.pointers.primary.off("up", this.onDragEnd);
   }
 
-  private onDragStart = (event: PointerEvent): void => {
-    this.isDragged = true;
-    this.dragStart = event.coordinates.worldPos;
+  private onPointerDown = (event: PointerEvent): void => {
+    if (!this.owner.isStill())
+      return;
+
+    if (this.isCharging) {
+      this.isCharging = false;
+      this.shoot();
+      return;
+    }
+    this.isCharging = true;
+    this.clickPosition = event.coordinates.worldPos;
   }
 
   private onPointerMove = (event: PointerEvent): void => {
-    if (!this.isDragged && !this.owner.isStill())
+    if (!this.owner.isStill())
       return;
-    const dragPos = event.coordinates.worldPos;
-    const ballPosition = this.owner.pos;
-    const cueDistToBall = this.isDragged ? this.dragStart.sub(dragPos).magnitude : 0
-    const dragDirection = ballPosition.sub(dragPos).normalize();
-    this.cue.pos = this.owner.pos.sub(dragDirection.scale(this.ballRadius + cueDistToBall))
-    this.cue.rotation = dragDirection.toAngle();
+    
+    if (this.isCharging) {
+      this.chargeShot(event.coordinates.worldPos);
+      return;
+    }
+    this.positionCue(event.coordinates.worldPos);
   }
 
-  private onDragEnd = (event: PointerEvent): void => {
-    if (!this.isDragged)
-      return;
-    this.dragEnd = event.coordinates.worldPos;
-    const dragDirection = this.owner.pos.sub(this.dragEnd).normalize();
-    this.isDragged = false;
+  private positionCue = (pointerPosition: Vector) => {
+    const ballPosition = this.owner.pos;
+    this.cueDirection = ballPosition.sub(pointerPosition).normalize();
+    this.cue.pos = ballPosition.sub(this.cueDirection.scale(this.ballRadius))
+    this.cue.rotation = this.cueDirection.toAngle();
+  }
+  
+  private chargeShot = (pointerPosition: Vector) => {
+    const ballPosition = this.owner.pos;
+    this.chargingStrength = this.clickPosition.sub(pointerPosition).magnitude;
+    this.cue.pos = ballPosition.sub(this.cueDirection.scale(this.ballRadius + this.chargingStrength));
+  }
+
+  private shoot() {
     this.cue.actions.moveTo({
-      pos: this.owner.pos.sub(dragDirection.scale(this.ballRadius)),
-      duration: 20 // duration of the shot, from letting cue go to hitting the ball
+      pos: this.owner.pos.sub(this.cueDirection.scale(this.ballRadius)),
+      duration: 30 // duration of the shot, from starting shot to hitting the ball
     }).callMethod(this.applyVelocity);
   }
 
   private applyVelocity = (): void => {
-    const direction = this.owner.pos.sub(this.dragEnd).normalize();
-    const strength = this.dragStart.sub(this.dragEnd).magnitude
-    const velocity = direction.scale(strength * 10); // arbitrary multiplier for shooting strength
+    const velocity = this.cueDirection.scale(this.chargingStrength * 10); // arbitrary multiplier for shooting strength
     this.owner.vel = velocity;
   }
 }
